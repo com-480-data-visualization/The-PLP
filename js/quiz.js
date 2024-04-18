0f7fcee0356c387ceb634ef45dd54e4d9f252292
valentinpey <127399575+valentinpey@users.noreply.github.com>
2024-04-18 15:53:15 +0200

    chnges on the quizz part add of a new pages and a start button

--- js/quiz.js
+++ js/quiz.js
@@ -1,32 +1,29 @@
+var map;  // Variable globale pour la carte
+
 document.addEventListener("DOMContentLoaded", function() {
-    const map = initializeMap();
-    addMapBounds(map);
-    
+    if (!map) {  // Vérifie si la carte n'a pas encore été initialisée
+        map = initializeMap();
+        addMapBounds(map);
+        document.getElementById('map').style.display = 'block';  // Afficher la carte
+    }
 });
 
+
 function initializeMap() {
-    // Initialise et retourne une carte Leaflet centrée avec un zoom initial
     var map = L.map('map', {
         center: [20, 0],
         zoom: 2,
-        maxBounds: [[-90, -180], [90, 180]],  // Limite pour ne pas "défiler à l'infini"
-        maxBoundsViscosity: 1.0,  // Empêche l'utilisateur de se déplacer hors des limites
-        worldCopyJump: false // Empêche la carte de se "boucler" lors du défilement
+        maxBounds: [[-90, -180], [90, 180]],
+        maxBoundsViscosity: 1.0,
+        worldCopyJump: false
     });
 
-    // Ajout du fond de carte
     L.esri.basemapLayer('Imagery').addTo(map);
-    //L.esri.basemapLayer('ImageryLabels').addTo(map);
-
-    
     return map;
 }
 
-
 function addMapBounds(map) {
-    // Empêche la carte de se "boucler" en définissant des limites strictes
     map.on('drag', () => {
         map.panInsideBounds([[-90, -180], [90, 180]], { animate: false });
     });
 }
-
